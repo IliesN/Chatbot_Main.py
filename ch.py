68fa34bbefa35b618b222362c9c7eed562367468
sssajin <151762061+sssajin@users.noreply.github.com>
2023-11-26 19:27:57 +0100

    Add files via upload

---
+++ ch.py
@@ -0,0 +1,30 @@
+while True:
+    print("Menu principal:")
+    print("1. Afficher la liste des mots les moins importants dans le corpus de documents.")
+    print("2. Afficher les mots ayant le score TD-IDF le plus élevé.")
+    print("3. Indiquer les mots les plus répétés par le président Chirac.")
+    print("4. Indiquer les noms des présidents qui ont parlé de la « Nation » et celui qui l’a répété le plus de fois.")
+    print("5. Indiquer le premier président à parler du climat et/ou de l’écologie.")
+    print("6. Hormis les mots dits « non importants », quels sont les mots que tous les présidents ont évoqués.")
+    choix = int(input("Choisissez une option entre 1 et 6 :"))
+
+    if choix =='1':
+        print("La liste des mots les moins importants sont :", .......)
+
+    elif choice == '2':
+        #
+
+    elif choice == '3':
+        #
+
+    elif choice == '4':
+        #
+
+    elif choice == '5':
+        #
+
+    elif choice == '6':
+        #
+
+    else:
+        print("Choix invalide. Veuillez choisir une option de 0 à 6.")
