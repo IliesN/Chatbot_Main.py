d0c0429767692cdbde8d25bded1abb254ca64f98
IliesN <151048681+IliesN@users.noreply.github.com>
2023-11-26 23:50:23 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,30 +1,27 @@
+import Core
+import time
 while True:
     print("Menu principal:")
     print("1. Afficher la liste des mots les moins importants dans le corpus de documents.")
     print("2. Afficher les mots ayant le score TD-IDF le plus élevé.")
     print("3. Indiquer les mots les plus répétés par le président Chirac.")
-    print("4. Indiquer les noms des présidents qui ont parlé de la « Nation » et celui qui l’a répété le plus de fois.")
-    print("5. Indiquer le premier président à parler du climat et/ou de l’écologie.")
-    print("6. Hormis les mots dits « non importants », quels sont les mots que tous les présidents ont évoqués.")
-    choix = int(input("Choisissez une option entre 1 et 6 :"))
+    print("4. Indiquer les noms des présidents qui ont parlé de la « Nation »")
+    choix = int(input("Choisissez un nombre entre 1 et 4 : "))
 
-    if choix =='1':
-        print("La liste des mots les moins importants sont :", .......)
-
-    elif choice == '2':
-        #
-
-    elif choice == '3':
-        #
-
-    elif choice == '4':
-        #
-
-    elif choice == '5':
-        #
-
-    elif choice == '6':
-        #
-
+    if choix == 1:
+        print("Question 1 sélectionnée. Veuillez patienter...")
+        print("La liste des mots les moins importants sont :", Core.ex1())
+        time.sleep(5)
+    elif choix == 2:
+        print("Question 2 sélectionnée. Veuillez patienter...")
+        print("La liste des mots ayant le score TD-IDF le plus élevé est :", Core.ex2())
+        time.sleep(5)
+    elif choix == 3:
+        print("Les mots les plus répétés par le président Chirac sont :", Core.ex3())
+        time.sleep(5)
+    elif choix == 4:
+        print(Core.ex4())
+        time.sleep(5)
     else:
-        print("Choix invalide. Veuillez choisir une option de 0 à 6.")
+        print("Choix invalide. Veuillez choisir une option de 1 à 4.")
+        time.sleep(3)
