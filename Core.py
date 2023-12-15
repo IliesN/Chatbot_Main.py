2bf1a36f1fadd0fc4722ef1fa07678678a6ca013
sssajin <151762061+sssajin@users.noreply.github.com>
2023-12-15 13:11:40 +0000

    Mise à jour de Core.py
    Ajout de 
    - calcul de la similarité
    
    Modification du fonctionnement de la matrice : 
    - rendu légèrement plus optimisé (with open, variables redondantes supprimées)

--- Core.py
+++ Core.py
@@ -1,7 +1,8 @@
 import os
 import math
 
-def list_of_files(directory, extension):
+
+def liste_fichiers(directory, extension):
     files_names = []
     for filename in os.listdir(directory):
         if filename.endswith(extension):
@@ -9,52 +10,52 @@ def list_of_files(directory, extension):
     return files_names
 
 
-prez_file = list_of_files("speeches-20231108", ".txt")
-nfile = len(prez_file)
-prez_name = {"Jacques": "Chirac",
+prez_fichiers = liste_fichiers("speeches-20231108", ".txt")
+nfile = len(prez_fichiers)
+prez_nom = {"Jacques": "Chirac",
              "Valéry": "Giscard dEstaing",
              "François": "Hollande",
              "Emmanuel": "Macron",
              "françois": "Mitterand",
              "Nicolas": "Sarkozy"}
 
-def prez_lastname():
+
+def prez_nomfamille():
     prez_lastnam = []
-    for line in prez_file:
-        j = 0
-        for char in line:
-            if char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
-                break
-
-            else:
-                j += 1
-
-        if (line[11:j]) not in prez_lastnam:
-            prez_lastnam.append(line[11:j])
+    for filename in prez_fichiers:
+        with open("speeches-20231108/" + filename, "r") as file:
+            j = 0
+            for char in file.read():
+                if char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
+                    break
+                else:
+                    j += 1
+            if (file.read(11)[11:j]) not in prez_lastnam:
+                prez_lastnam.append(file.read(11)[11:j])
     return prez_lastnam
 
 
 def convert():
-    for element in prez_file:
+    for element in prez_fichiers:
         r = open("speeches-20231108/"+element, "r")
         c = open("Cleaned/"+element, "w")
-        for ligne in r.readlines():
-            for charac in ligne:
-                if ord("A") <= ord(charac) <= ord("Z"):
-                    c.write(chr(ord(charac)+((ord("a")-ord("A")))))
-                elif ord(charac) == ord("'"):
+        for line in r.readlines():
+            for character in line:
+                if ord("A") <= ord(character) <= ord("Z"):
+                    c.write(chr(ord(character)+(ord("a")-ord("A"))))
+                elif ord(character) == ord("'"):
                     c.write("e ")
-                elif ord(charac) == ord("-") or ord(charac) == ord("_"):
+                elif ord(character) == ord("-") or ord(character) == ord("_"):
                     c.write(" ")
-                elif ord(",") <= ord(charac) <= ord(".") or ord(":") <= ord(charac) <= ord(";") or ord(charac) == ord("?") or ord(charac) == 33:
-
+                elif (ord(",") <= ord(character) <= ord(".")
+                      or ord(":") <= ord(character) <= ord(";")
+                      or ord(character) == ord("?") or ord(character) == 33):
                     c.write("")
                 else:
-                    c.write(charac)
+                    c.write(character)
 
 
-
-def tf(string):
+def score_tf(string):
     dic = {}
     if type(string) == list:
         for i in range(len(string)):
@@ -74,77 +75,95 @@ def tf(string):
                 dic[a[i]] = 1
         return dic
 
-def idf():
+
+def score_idf():
     dic = {}
     dicidf = {}
-    for element in prez_file:
-        c = open("Cleaned/" + element, "r")
-        for ligne in c.readlines():
-            for lr in ligne.splitlines():
-                a = lr.split(" ")
-                for i in range(len(a)):
-                    if a[i] in dic and a[i] != "":
-                        dic[a[i]] += 1
-                    elif a[i] not in dic and a[i] != "":
-                        dic[a[i]] = 1
 
-    for element2 in prez_file:
+    for element in prez_fichiers:
+        with open("Cleaned/" + element, "r") as c:
+            for ligne in c.readlines():
+                for lr in ligne.splitlines():
+                    a = lr.split(" ")
+                    for i in range(len(a)):
+                        if a[i] in dic and a[i] != "":
+                            dic[a[i]] += 1
+                        elif a[i] not in dic and a[i] != "":
+                            dic[a[i]] = 1
+
+    for element2 in prez_fichiers:
         tempdic = {}
-        d = open("Cleaned/" + element2, "r")
-        for ligne2 in d.readlines():
-            for lr in ligne2.splitlines():
-                b = lr.split(" ")
-                for mot in dic:
-                    if mot in b:
-                        tempdic[mot] = 1
+        with open("Cleaned/" + element2, "r") as d:
+            for ligne2 in d.readlines():
+                for lr in ligne2.splitlines():
+                    b = lr.split(" ")
+                    for mot in dic:
+                        if mot in b:
+                            tempdic[mot] = 1
+
         for el in tempdic:
-            if el in dicidf:
-                dicidf[el] += 1
-            else:
-                dicidf[el] = 1
+            dicidf[el] = dicidf.get(el, 0) + 1
+
     for ele in dicidf:
-        dicidf[ele] = math.log10(nfile/dicidf[ele])
+        dicidf[ele] = math.log10(nfile / dicidf[ele])
 
     return dicidf
 
 
-
-
-def tf_idf(direct):
-    idfdic = idf()
+def tf_idf_matrix():
+    idfdic = score_idf()
     mtfidf = []
-    for element in idfdic:
+
+    for element in prez_fichiers:
         newlist = []
-        for j in range(8):
-            f = open(direct+"/"+prez_file[j])
+        with open("Cleaned/" + element) as f:
             readfile = f.read()
-            if element in tf(readfile):
-                newlist.append(round(tf(readfile)[element]*idfdic[element], 2))
-            else:
-                newlist.append(0.0)
+            for e in idfdic:
+                if e in score_tf(readfile):
+                    newlist.append(round(score_tf(readfile)[e] * idfdic[e], 2))
+                else:
+                    newlist.append(0.0)
         mtfidf.append(newlist)
 
-    return idfdic, mtfidf
+    return mtfidf
 
-def print_mtfidf():
-    idfdic = idf()
-    mtfidf = tf_idf("Cleaned")[1]
+
+
+def print_tfidf_matrix():
+    mtfidf = tf_idf_matrix()
+    idfdic = score_idf()
     x = 0
-    for element in idfdic:
-        print(mtfidf[x], element)
-        x+=1
+    print(" "*34, end=" ")
+    maxlen = 0
+    for word in idfdic:
+        if len(word) > maxlen:
+            maxlen=len(word)
+        print(word, " "*(34-len(word)), end= " ")
+    print(maxlen)
+    print("\n")
+    for element in prez_fichiers:
+        print(element, " "*(32-len(element)), end =" ")
+        for value in mtfidf[x]:
+            if len(str(value)) == 2:
+                print(value," "*32, end= " ")
+            elif len(str(value)) == 3:
+                print(value," "*31, end= " ")
+            elif len(str(value)) == 4:
+                print(value," "*30, end= " ")
+        print("\n")
+        x += 1
 
-    print(x, " mots")
+
 
 def ex1():
-    idfdic = idf()
-    matrix = tf_idf("Cleaned")[1]
+    idfdic = score_idf()
+    matrix = tf_idf_matrix()
     uniml = []
     x = 0
     for element in idfdic:
         unim = True
-        for i in range(len(matrix[x])):
-            if matrix[x][i] != 0.0:
+        for i in range(8):
+            if matrix[i][x] != 0.0:
                 unim = False
         if unim:
             uniml.append(element)
@@ -153,25 +172,28 @@ def ex1():
 
 
 def ex2():
-    idfdic = idf()
-    matrix = tf_idf("Cleaned")[1]
+    idfdic = score_idf()
+    matrix = tf_idf_matrix()
     maximum = 0.0
     maxlist = []
     x = 0
-    for element in idfdic:
-        for i in range(len(matrix[x])):
-            if matrix[x][i] > maximum:
-                maximum = matrix[x][i]
-        x+=1
-    x=0
-    for element in idfdic:
-        if maximum in matrix[x]:
-            maxlist.append(element)
+    for _ in idfdic:
+        for i in range(8):
+            if matrix[i][x] > maximum:
+                maximum = matrix[i][x]
         x += 1
-    if len(maxlist)==1:
+    for i in range(8):
+        x=0
+        for element in idfdic:
+            if maximum == matrix[i][x]:
+                maxlist.append(element)
+            x+=1
+        x += 1
+    if len(maxlist) == 1:
         return maxlist[0]
     else:
         return maxlist
+
 
 def ex3():
     unimportant_words = ex1()
@@ -179,94 +201,92 @@ def ex3():
     max1w = ""
     max2 = 0
     max2w = ""
-    f = open("Cleaned/Nomination_Chirac1.txt", "r")
-    read1 = f.read()
-    dic1 = tf(read1)
-    g = open("Cleaned/Nomination_Chirac2.txt", "r")
-    read2 = g.read()
-    dic2 = tf(read1)
+    with open("Cleaned/Nomination_Chirac1.txt", "r") as file:
+        read1 = file.read()
+        dic1 = score_tf(read1)
+        dic2 = score_tf(read1)
 
-    for element in dic1:
+        for element in dic1:
+            if dic1[element] > max1 and element != '' and element not in unimportant_words:
+                max1 = dic1[element]
+                max1w = element
+        for element in dic2:
+            if dic2[element] > max2 and element != '' and element not in unimportant_words:
+                max2 = dic2[element]
+                max2w = element
+        if max1w == max2w:
+            return max1w
+        else:
+            return max1w, max2w
 
-        if dic1[element] > max1 and element != '' and element not in unimportant_words:
-
-            max1 = dic1[element]
-            max1w = element
-    for element in dic2:
-        if dic2[element] > max2 and element != '' and element not in unimportant_words:
-            max2 = dic2[element]
-            max2w = element
-    if max1w == max2w:
-        return max1w
-    else:
-        return max1w, max2w
 
 def ex4():
     listprez = []
-    nationmax = 0
-    for element in prez_file:
+    for element in prez_fichiers:
         f = open("Cleaned/"+element)
         reading = f.read()
-        dic = tf(reading)
+        dic = score_tf(reading)
         if "nation" in dic and element.strip("Nomination_").strip(".txt").strip("1").strip("2") not in listprez:
             listprez.append(element.strip("Nomination_").strip(".txt").strip("1").strip("2"))
     return listprez
 
+
 def ex5():
     premier = True
     premierprez = []
-    for element in prez_file:
+    for element in prez_fichiers:
         f = open("Cleaned/"+element)
         reading = f.read()
-        dic = tf(reading)
+        dic = score_tf(reading)
         while premier:
-            if "climat" or "écologie" in dic :
+            if "climat" or "écologie" in dic:
                 premierprez.append(element.strip("Nomination_").strip(".txt").strip("1").strip("2"))
                 premier = False
     return premierprez[0]
 
+
 def ex6():
     dic = {}
-    basewordlist = []
     unim = ex1()
     f = open("Cleaned/Nomination_Chirac1.txt")
     reading = f.read()
     f.close()
     basewordlist = reading.split()
-    for element in prez_file:
+    for element in prez_fichiers:
         f = open("Cleaned/"+element)
         reading = f.read()
         wordlist = reading.split()
-        for element in basewordlist:
-            if element not in wordlist:
-                basewordlist.remove(element)
+        for element2 in basewordlist:
+            if element2 not in wordlist:
+                basewordlist.remove(element2)
     for i in range(len(basewordlist)):
         if basewordlist[i] in dic:
             dic[basewordlist[i]] += 1
         else:
             dic[basewordlist[i]] = 1
     basewordlist = []
-    for element in dic:
-        if element not in unim:
-            basewordlist.append(element)
+    for element3 in dic:
+        if element3 not in unim:
+            basewordlist.append(element3)
     return basewordlist
+
 
 def tokenisation(string):
     motvide = ["le", "de", "et", "la", "à", "est", "les", "pour", "un", "une"]
     finalstr = ""
     for charac in string:
         if ord("A") <= ord(charac) <= ord("Z"):
-            charac = chr(ord(charac)+((ord("a")-ord("A"))))
-            finalstr+=charac
+            charac = chr(ord(charac)+(ord("a")-ord("A")))
+            finalstr += charac
         elif ord(charac) == ord("'"):
-            charac = ("e ")
+            charac = "e "
             finalstr += charac
         elif ord(charac) == ord("-") or ord(charac) == ord("_"):
-            charac = (" ")
+            charac = " "
             finalstr += charac
         elif ord(",") <= ord(charac) <= ord(".") or ord(":") <= ord(charac) <= ord(";") or ord(charac) == ord("?") or ord(charac) == 33:
 
-            charac = ("")
+            charac = ""
             finalstr += charac
         else:
             finalstr += charac
@@ -276,24 +296,66 @@ def tokenisation(string):
             exstr.remove(element)
     return exstr
 
+
 def whichincorpus(q):
-    wordic = tf_idf("Cleaned")[0]
+    wordic = score_idf()
     wordlist = tokenisation(q)
     for element in wordlist:
         if element not in wordic.keys():
             wordlist.remove(element)
     print(wordlist)
 
-def q_tf(string):
+
+def question_tf(string):
     string2 = tokenisation(string)
-    l = len(string2)
-    dictfq = tf(string2)
-    for element in dictfq:
-        dictfq[element]/=l
+    dictfq = score_tf(string2)
     return dictfq
 
 
+def question_tf_idf(string):
+    qlist = []
+    tf_s = question_tf(string)
+    idfs = score_idf()
+    for element in tf_s:
+        if element in idfs:
+            qlist.append(round(tf_s[element]*idfs[element], 2))
+        else:
+            qlist.append(0.0)
+    return qlist
 
 
 
+def produit_scalaire(A, B):
+    scal = 0.0
+    for i in range(len(A)):
+        scal += A[i]*B[i]
+    return scal
 
+
+def norme(A):
+    normv = 0.0
+    for element in A:
+        normv+=element**2
+    normv = math.sqrt(normv)
+    return normv
+
+
+def calcul_similarite(A, B):
+    sim = produit_scalaire(A,B)/norme(A)*norme(B)
+    return sim
+
+def calcul_pertinent(corpus, V_question, listenoms):
+    maximum = 0.0
+    filename = ""
+    x=0
+    for element in listenoms:
+        max_len = len(corpus[x])
+        V_question += [0.0] * (max_len - len(V_question))
+        sim = calcul_similarite(corpus[x], V_question)
+
+        if sim > maximum:
+            maximum = sim
+
+            filename = element
+        x+=1
+    return filename
