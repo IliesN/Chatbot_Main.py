8680e5abc45d38267924c313411d407f4a3b12bd
sssajin <151762061+sssajin@users.noreply.github.com>
2023-12-17 21:08:57 +0000

    Update Core.py

--- Core.py
+++ Core.py
@@ -1,24 +1,19 @@
 import os
 import math
 
-# fonction qui permet de parcourir la liste des fichiers d’une extension donnée et dans un répertoire donné.
-def liste_fichiers(directory, extension): 
-    #créaton d'une liste files_names pour stocker les noms de chaque fichier.
-    files_names = []
-    # Parcours tous le fichiers dans le répertoire spécifié
-    for filename in os.listdir(directory):
-        # Vérifie si le nom du fichier se termine par l'extension
-        if filename.endswith(extension):
+
+def liste_fichiers(directory, extension):                             # fonction qui permet de parcourir la liste des fichiers d’une extension donnée et dans un répertoire donné.
+    files_names = []                                                  #créaton d'une liste files_names pour stocker les noms de chaque fichier.
+    for filename in os.listdir(directory):                            # Parcours tous le fichiers dans le répertoire spécifié
+        if filename.endswith(extension):                              # Vérifie si le nom du fichier se termine par l'extension
             files_names.append(filename)
-    #Retroune la liste des noms de fichier
     return files_names
 
-#Utilisation de la fonction liste_fichiers pour obtenir la liste des fichiers avec l'extension .txt 
-prez_fichiers = liste_fichiers("speeches-20231108", ".txt")
+
+prez_fichiers = liste_fichiers("speeches-20231108", ".txt")           #Utilisation de la fonction liste_fichiers pour obtenir la liste des fichiers avec l'extension .txt 
 #Calcul le nombre de fichier de la liste prez_fichiers
 nfile = len(prez_fichiers)
-#Création d'un dictionnaire associant chaque prénom à des noms de dirigeants politiques
-prez_nom = {"Jacques": "Chirac",
+prez_nom = {"Jacques": "Chirac",                                      #Création d'un dictionnaire associant chaque prénom à des noms de dirigeants politiques
 
             "Valéry": "Giscard dEstaing",
             "François": "Hollande",
@@ -26,64 +21,52 @@ prez_nom = {"Jacques": "Chirac",
             "françois": "Mitterand",
             "Nicolas": "Sarkozy"}
 
-#Fonction pour stocker les noms de president dans une liste
-def prez_nomfamille():
+
+def prez_nomfamille():                                               #Fonction pour stocker les noms de president dans une liste
     prez_lastnam = []
     for filename in prez_fichiers:
-        # Ouverture du fichier en mode lecture
-        with open("speeches-20231108/" + filename, "r") as file:
+        with open("speeches-20231108/" + filename, "r") as file:     # Ouverture du fichier en mode lecture
             j = 0
             for char in file.read():
-                # Si le caractère est un chiffre ou un point, arrête la boucle
-                if char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
+                if char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:    # Si le caractère est un chiffre ou un point, arrête la boucle
                     break
                 else:
                     j += 1
-            #Lecture d'une portion du fichier pour obtenir le nom de famille puis vérifie si le nom est dans la liste prez_lastam
-            if (file.read(11)[11:j]) not in prez_lastnam:
+            if (file.read(11)[11:j]) not in prez_lastnam:           #Lecture d'une portion du fichier pour obtenir le nom de famille puis vérifie si le nom est dans la liste prez_lastam
                 prez_lastnam.append(file.read(11)[11:j])
     return prez_lastnam
 
 def convert():
     for element in prez_fichiers:
-        # Ouverture du fichier original en mode lecture (r) et du fichier nettoyé en mode écriture (w)
-        with open("speeches-20231108/"+element, "r") as r, open("Cleaned/"+element, "w") as c:
+        with open("speeches-20231108/"+element, "r") as r, open("Cleaned/"+element, "w") as c:     # Ouverture du fichier original en mode lecture (r) et du fichier nettoyé en mode écriture (w)
             for line in r.readlines():
                 for character in line:
-                    # Convertit les majuscules en minuscules
-                    if ord("A") <= ord(character) <= ord("Z"):
+                    if ord("A") <= ord(character) <= ord("Z"):                                     # Convertit les majuscules en minuscules
                         c.write(chr(ord(character)+(ord("a")-ord("A"))))
-                    #Remplace l'apostrophe par la lettre "e" et ajoute un espace
-                    elif ord(character) == ord("'"):
+                    elif ord(character) == ord("'"):                                               #Remplace l'apostrophe par la lettre "e" et ajoute un espace
                         c.write("e ")
-                    # Remplace le tiret ou le tiret du bas par un espace
-                    elif ord(character) == ord("-") or ord(character) == ord("_"):
+                    elif ord(character) == ord("-") or ord(character) == ord("_"):                 # Remplace le tiret ou le tiret du bas par un espace
                         c.write(" ")
-                    # Supprime la ponctuation spécifiée
+                                                                                                   # Supprime la ponctuation spécifiée
                     elif (ord(",") <= ord(character) <= ord(".") or ord(":") <= ord(character) <= ord(";") or ord(character) == ord("?") or ord(character) == 33):
                         c.write("")
                     else:
                         c.write(character)
 
 
-#focntion qui calcule le score tf de chaque mot grâce à un dictionnaire
-def score_tf(string):
-    # Initialisation du dictionnaire pour stocker les occurrences des mots
-    dic = {}
-    #Vérification, si l'entré est une liste
-    if type(string) == list:
+
+def score_tf(string):                                                                       #focntion qui calcule le score tf de chaque mot grâce à un dictionnaire
+    dic = {}                                                                                # Initialisation du dictionnaire pour stocker les occurrences des mots
+    if type(string) == list:                                                                #Vérification, si l'entré est une liste
         for i in range(len(string)):
-            # Vérification si le mot est déjà présent dans le dictionnaire
-            if string[i] in dic:
-                # Si oui, incrémentation du compteur
-                dic[string[i]] += 1
+            if string[i] in dic:                                                            #Vérifie si le mot est dans le dictionnaire                                    
+                dic[string[i]] += 1                                                         # Si oui, incrémentation du compteur
             else:
-                 #Si non, incrémentation du compteur à 1
-                dic[string[i]] = 1
+                dic[string[i]] = 1                                                          #Si non, incrémentation du compteur à 1
         return dic
 
     else:
-        # Si l'entrée est une chaîne de caractères, séparation des mots en utilisant l'espace comme délimiteur
+                                                                                            # Si l'entrée est une chaîne de caractères, séparation des mots en utilisant l'espace comme délimiteur
         b = string.replace('\n', ' ')
         a = b.split(" ")
         for i in range(len(a)):
@@ -93,18 +76,15 @@ def score_tf(string):
                 dic[a[i]] = 1
         return dic
 
-#focntion qui calcule le score idf grâce à un dictionnaire.
-def score_idf():
-     # Dictionnaire pour stocker les occurrences des mots dans tous les documents
-    dic = {}
-    # Dictionnaire pour stocker le score IDF pour chaque mot
-    dicidf = {}
+
+def score_idf():                                                                           #focntion qui calcule le score idf grâce à un dictionnaire.
+    dic = {}                                                                               # Dictionnaire pour stocker les occurrences des mots dans tous les documents
+    dicidf = {}                                                                            # Dictionnaire pour stocker le score IDF pour chaque mot
 
     for element in prez_fichiers:
         with open("Cleaned/" + element, "r") as c:
             for ligne in c.readlines():
-                # Division de la ligne en une liste de lignes
-                for lr in ligne.splitlines():
+                for lr in ligne.splitlines():                                              # Division de la ligne en une liste de lignes
                     a = lr.split(" ")
                     for i in range(len(a)):
                         if a[i] in dic and a[i] != "":
@@ -126,20 +106,17 @@ def score_idf():
                             tempdic[mot] = 1
 
         for el in tempdic:
-            # Incrémentation du compteur global du mot
-            dicidf[el] = dicidf.get(el, 0) + 1
+            dicidf[el] = dicidf.get(el, 0) + 1                                     # Incrémentation du compteur global du mot
 
     for ele in dicidf:
-        # Calcul du score IDF pour chaque mot
-        dicidf[ele] = math.log10(nfile / dicidf[ele])
+        dicidf[ele] = math.log10(nfile / dicidf[ele])                              # Calcul du score IDF pour chaque mot
 
     return dicidf
 
-#focntion qui permet de calculer les scores tfidf et de les stocker dans une matrice
-def tf_idf_matrix():
-    idfdic = score_idf()
-    #création d'une matrice tfidf
-    mtfidf = [] # Liste pour stocker les scores TF-IDF du document actuel
+
+def tf_idf_matrix():                                                               #focntion qui permet de calculer les scores tfidf et de les stocker dans une matrice
+    idfdic = score_idf()                                                           #création d'une matrice tfidf
+    mtfidf = []                                                                    # Liste pour stocker les scores TF-IDF du document actuel
         
 
     for element in prez_fichiers:
@@ -147,26 +124,23 @@ def tf_idf_matrix():
         with open("Cleaned/" + element) as f:
             readfile = f.read()
             for e in idfdic:
-                # Vérification de la présence du terme dans les scores TF
-                if e in score_tf(readfile):
-                    # Calcul du score TF-IDF et ajout à la liste
-                    newlist.append(round(score_tf(readfile)[e] * idfdic[e], 2))
+                if e in score_tf(readfile):                                        # Vérification de la présence du terme dans les scores TF
+                    newlist.append(round(score_tf(readfile)[e] * idfdic[e], 2))    # Calcul du score TF-IDF et ajout à la liste
                 else:
                     newlist.append(0.0)
         mtfidf.append(newlist)
 
-    return mtfidf    # Retour de la matrice TF-IDF complète
+    return mtfidf                                                                  # Retour de la matrice TF-IDF complète
 
   
-def print_tfidf_matrix(): #fonction qui permet d'afficher la matrice 
+def print_tfidf_matrix():                        #fonction qui permet d'afficher la matrice 
     mtfidf = tf_idf_matrix()
     idfdic = score_idf()
     x = 0
     print(" "*34, end=" ")
     maxlen = 0
     for word in idfdic:
-        # Mise à jour de la longueur maximale si nécessaire
-        if len(word) > maxlen:
+        if len(word) > maxlen:                   # Mise à jour de la longueur maximale si nécessaire
             maxlen = len(word)
         print(word, " "*(34-len(word)), end=" ") # Affichage du mot avec un espacement approprié
     print(maxlen)
@@ -175,7 +149,7 @@ def print_tfidf_matrix(): #fonction qui permet d'afficher la matrice
         print(element, " "*(32-len(element)), end=" ")
         for value in mtfidf[x]:
             if len(str(value)) == 2:
-                print(value, " "*32, end=" ") # Affichage de la valeur avec un espacement approprié basé sur la longueur du nombre
+                print(value, " "*32, end=" ")   # Affichage de la valeur avec un espacement approprié basé sur la longueur du nombre
             elif len(str(value)) == 3:
                 print(value, " "*31, end=" ")
             elif len(str(value)) == 4:
@@ -187,7 +161,7 @@ def print_tfidf_matrix(): #fonction qui permet d'afficher la matrice
 def ex1():
     idfdic = score_idf()
     matrix = tf_idf_matrix()
-    uniml = []                  # Liste pour stocker les termes uniques
+    uniml = []                       # Liste pour stocker les termes uniques
     x = 0                       
     for element in idfdic:
         unim = True
@@ -205,7 +179,7 @@ def ex2():
     matrix = tf_idf_matrix()
     maximum = 0.0
     maxlist = []
-    x = 0                     # Initialisation de la valeur maximale
+    x = 0                               # Initialisation de la valeur maximale
     for _ in idfdic:
         for i in range(8):
             if matrix[i][x] > maximum:  # Vérification si la valeur est supérieure à la valeur maximale actuelle
@@ -268,7 +242,7 @@ def ex5():
         reading = f.read()
         dic = score_tf(reading)
         if "climat" or "écologie" in dic:
-            if element.strip("Nomination_").strip(".txt").strip("1").strip("2") not in prez:
+            if element.strip("Nomination_").strip(".txt").strip("1").strip("2") not in prez:             #Vérification de la présence du mot "climat" ou "écologie" dans le dictionnaire et exclusion des duplicatas
                 prez.append(element.strip("Nomination_").strip(".txt").strip("1").strip("2"))
     return prez
 
@@ -299,43 +273,43 @@ def ex6():
 
 
 def tokenisation(string):
-    motvide = ["le", "de", "et", "la", "à", "est", "les", "pour", "un", "une"]
+    motvide = ["le", "de", "et", "la", "à", "est", "les", "pour", "un", "une"]  
     motquestion = ["comment", "pourquoi"]
     finalstr = ""
     for charac in string:
-        if ord("A") <= ord(charac) <= ord("Z"):
+        if ord("A") <= ord(charac) <= ord("Z"):                          # Convertit les lettres majuscules en minuscules
             charac = chr(ord(charac)+(ord("a")-ord("A")))
             finalstr += charac
-        elif ord(charac) == ord("'"):
-            charac = "e "
+        elif ord(charac) == ord("'"):                    
+            charac = "e "                                                # Remplace les apostrophes par 'e'
             finalstr += charac
         elif ord(charac) == ord("-") or ord(charac) == ord("_"):
-            charac = " "
+            charac = " "                                                 # Remplace les tirets et underscores par des espaces
             finalstr += charac
         elif (ord(",") <= ord(charac) <= ord(".") or ord(":") <= ord(charac) <= ord(";") or ord(charac) == ord("?")
               or ord(charac) == 33):
 
-            charac = ""
+            charac = ""                                                  # Supprime certains caractères de ponctuation
             finalstr += charac
         else:
             finalstr += charac
-    exstr = finalstr.split()
+    exstr = finalstr.split()                                             # Divise la chaîne traitée en une liste de mots
     for element in exstr:
-        if element in motvide or element in motquestion:
+        if element in motvide or element in motquestion:                 # Crée une nouvelle liste avec les mots qui ne sont pas dans motvide et motquestion
             exstr.remove(element)
     return exstr
 
 
 def whichincorpus(q):
     wordic = score_idf()
-    wordlist = tokenisation(q)
+    wordlist = tokenisation(q)                                          # Tokenise la question
     for element in wordlist:
-        if element not in wordic.keys():
+        if element not in wordic.keys():                                # Vérifie si l'élément n'est pas dans le dictionnaire des scores IDF
             wordlist.remove(element)
     print(wordlist)
 
 
-def question_tf(string):
+def question_tf(string):                                               #Fonction qui permet de retourner un dictionnaire des scores TF après tokenisation de la question
     string2 = tokenisation(string)
     dictfq = score_tf(string2)
     return dictfq
@@ -345,55 +319,58 @@ def question_tf_idf(string):
     qlist = []
     tf_s = question_tf(string)
     idfs = score_idf()
-    for element in tf_s:
-        if element in idfs:
-            qlist.append(round(tf_s[element]*idfs[element], 2))
+    for element in tf_s:                                              
+        if element in idfs:                                           # Vérifie si l'élément est présent dans les scores IDF
+            qlist.append(round(tf_s[element]*idfs[element], 2))       # Calcul du score TF-IDF et ajoute à la liste
         else:
-            qlist.append(0.0)
+            qlist.append(0.0)                                         
 
-    return qlist, tokenisation(string)[qlist.index(max(qlist))]
+    return qlist, tokenisation(string)[qlist.index(max(qlist))]       # Retourne la liste des scores TF-IDF et le terme ayant le score maximal
 
 
 def produit_scalaire(a, b):
-    scal = 0.0
+    scal = 0.0                                                        # Initialisation de la variable pour stocker le résultat du produit scalaire
     for i in range(len(a)):
-        scal += a[i]*b[i]
+        scal += a[i]*b[i]                                             # Ajout du produit de chaque paire d'éléments correspondants
     return scal
 
 
 def norme(a):
     normv = 0.0
     for element in a:
-        normv += element**2
-    normv = math.sqrt(normv)
+        normv += element**2                                         
+    normv = 0.0
+    for element in a:
+        normv += element**2                                          # Ajoute le carré de chaque élément à la somme
+    normv = math.sqrt(normv)                                         # Calcul de la racine carrée de la somme des carrés pour obtenir la norme
     return normv
 
 
 def calcul_similarite(a, b):
-    sim = produit_scalaire(a, b)/norme(a)*norme(b)
+    sim = produit_scalaire(a, b)/norme(a)*norme(b)                  # Calcul de la similarité cosinus en utilisant la formule (a . b) / (|a| * |b|)
     return sim
 
 
-def calcul_pertinent(corpus, v_question, listenoms):
+def calcul_pertinent(corpus, v_question, listenoms):                # Focntion qui retourne le nom du document le plus pertinent 
 
     maximum = 0.0
     vlist = v_question[0]
     filename = ""
     mot_pertinent = v_question[1]
 
-    x = 0
+    x = 0                                                           # Compteur pour parcourir les éléments dans listenoms
     for element in listenoms:
         max_len = len(corpus[x])
-        vlist += [0.0] * (max_len - len(vlist))
-        sim = calcul_similarite(corpus[x], vlist)
+        vlist += [0.0] * (max_len - len(vlist))                     # Ajout de zéros pour aligner la longueur du vecteur avec celui du corpus
+        sim = calcul_similarite(corpus[x], vlist)                   # Calcul  la similarité  entre le vecteur question et le vecteur du corpus
 
-        if sim > maximum:
+        if sim > maximum:                                           # Mise à jour du maximum et du nom du fichier si la similarité actuelle est plus grande
             maximum = sim
             filename = element
 
         x += 1
 
-    return "speeches-20231108/"+filename, mot_pertinent, maximum
+    return "speeches-20231108/"+filename, mot_pertinent, maximum   # Retourne le résultat final (nom du fichier, mot pertinent, et similarité maximale)
 
 def phrase(fichier):
 
